74e654024d15e7609c71e0bc5bb8476978f5a921
argentol <ivchernyavski@mail.ru>
2021-04-19 18:52:31 +0300

    Bugs fixed

---
+++ playtest 3d unity/Assets/Scripts/Coloring.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Coloring : MonoBehaviour, IPointerDownHandler
+{
+    public GameObject Body;
+    public GameObject Legs;
+    public Material Material;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Body.GetComponent<Renderer>().material.color = Material.color;
+        Legs.GetComponent<Renderer>().material.color = Material.color;
+    }
+}
